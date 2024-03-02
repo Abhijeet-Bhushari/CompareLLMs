9df8d8e7280d381c8b207a9cad5a379caa7925de
Abhijeet-Bhushari <115354940+Abhijeet-Bhushari@users.noreply.github.com>
2024-03-02 07:14:36 +0530

    index updated

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask_ngrok import run_with_ngrok
 from flask import Flask, render_template, request
 
-import torch
-from diffusers import StableDiffusionPipeline
+# import torch
+# from diffusers import StableDiffusionPipeline
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 import base64
@@ -12,8 +12,8 @@ from io import BytesIO
 tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xl")
 model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xl").to("cuda")
 
-pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", revision="fp16", torch_dtype=torch.float16)
-pipe.to("cuda")
+# pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", revision="fp16", torch_dtype=torch.float16)
+# pipe.to("cuda")
 
 # Start flask app and set to ngrok
 app = Flask(__name__)
@@ -30,22 +30,22 @@ def generate():
   prompt = request.form['prompt-input']
   print(f"Generating an image of {prompt}")
 
-  # generate image
-  image = pipe(prompt).images[0]
-  print("Image generated! Converting image ...")
-  buffered = BytesIO()
-  image.save(buffered, format="PNG")
-  img_str = base64.b64encode(buffered.getvalue())
-  img_str = "data:image/png;base64," + str(img_str)[2:-1]
+  # # generate image
+  # image = pipe(prompt).images[0]
+  # print("Image generated! Converting image ...")
+  # buffered = BytesIO()
+  # image.save(buffered, format="PNG")
+  # img_str = base64.b64encode(buffered.getvalue())
+  # img_str = "data:image/png;base64," + str(img_str)[2:-1]
 
   #generate text
   input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
   generated_output = model.generate(input_ids, do_sample=True, temperature=1.0, max_length=2500, num_return_sequences=1)
   generated_text = tokenizer.decode(generated_output[0], skip_special_tokens=True)
 
-  print("Sending image and text ...")
+  print("Sending text ...")
  
-  return render_template('index.html', generated_image=img_str, generated_text=generated_text, prompt=prompt)
+  return render_template('index.html', generated_text=generated_text, prompt=prompt)
 
 if __name__ == '__main__':
     app.run()
